f89b9f543950793dd4e3c7b5d1f553f7f6ae0a56
Sebastian Glahn <hisgla.hn>
2018-09-26 13:24:03 +0200

    Migrated to Python3

--- server/server.py
+++ server/server.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 '''
 
@@ -23,14 +23,16 @@ import argparse
 import hashlib
 import glob
 import logging
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import ssl
+import http.server
 
+CERT_FILE = None
 LOG_LEVEL = 'ERROR'
 LOG_FORMAT = '%(asctime)-15s %(levelname)s %(ip)s --- %(message)s'
 PORT_NUMBER = 8000
 FIRMWARE_DIRECTORY = os.environ['HOME'] + os.sep + "firmware"
 
-class HttpHandler(BaseHTTPRequestHandler):
+class HttpHandler(http.server.BaseHTTPRequestHandler):
 
     def getLatestFirmwareVersion(self, flavor):
         for firmware in os.listdir(FIRMWARE_DIRECTORY):
@@ -41,12 +43,11 @@ class HttpHandler(BaseHTTPRequestHandler):
     def validRequest(self, flavor):
         return glob.glob(FIRMWARE_DIRECTORY + os.sep + flavor + '*') and 'x-ESP8266-version' in self.headers
 
-    def buildHtmlResponse(self, status, text=''):
+    def buildHtmlResponse(self, status):
         self.send_response(status)
-        self.send_header('Content-type', 'text/html')
+        self.send_header('Content-type', 'text/html; charset=utf-8')
         self.end_headers()
-        self.wfile.write(text)
-
+       
     def buildStreamResponse(self, flavor, latest):
         filename = FIRMWARE_DIRECTORY + os.sep + flavor + '-' + latest + ".bin"
         self.send_response(200)
@@ -55,9 +56,8 @@ class HttpHandler(BaseHTTPRequestHandler):
         self.send_header('Content-Length', os.path.getsize(filename))
         self.send_header('x-MD5', hashlib.md5(open(filename, 'rb').read()).hexdigest())
         self.end_headers()
-        binary = open(filename) 
-        self.wfile.write(binary.read())
-        binary.close()
+        with open(filename, 'rb') as binary:
+            self.wfile.write(binary.read())
 
     def do_GET(self):
         log_stat = { 'ip' : self.client_address[0] }
@@ -65,7 +65,7 @@ class HttpHandler(BaseHTTPRequestHandler):
 
         if not self.validRequest(flavor):
             logging.error('Invalid request', extra = log_stat)
-            self.buildHtmlResponse(400, 'Invalid request')
+            self.buildHtmlResponse(400)
             return
         
         latest = self.getLatestFirmwareVersion(flavor)
@@ -83,6 +83,7 @@ parser = argparse.ArgumentParser(description='HTTP Server which delivers firmwar
 parser.add_argument('--dir', help='Directory containing the firmware binaries to serve. Default: ~/firmware')
 parser.add_argument('--port', help='Server port. Default: 8000.')
 parser.add_argument('--log', help='Log level. Default ERROR')
+parser.add_argument('--cert', help='SSL cert file to enable HTTPS. Default empty=No HTTPS')
 args = parser.parse_args()
 
 if args.dir:
@@ -91,13 +92,21 @@ if args.port:
     PORT_NUMBER = int(args.port)
 if args.log:
     LOG_LEVEL = args.log.upper()
+if args.cert:
+    CERT_FILE = args.cert    
+
+    
+
 logging.basicConfig(level=LOG_LEVEL, format=LOG_FORMAT)
 
 try:
-    server = HTTPServer(('', PORT_NUMBER), HttpHandler)
-    print 'Started httpserver on port ' + str(PORT_NUMBER) + ', firmware directory: ' + FIRMWARE_DIRECTORY
+    server = http.server.HTTPServer(('localhost', PORT_NUMBER), HttpHandler)
+    if CERT_FILE:
+        server.socket = ssl.wrap_socket(server.socket, certfile='server.pem', server_side=True)
+
+    print('Started httpserver on port ' + str(PORT_NUMBER) + ', firmware directory: ' + FIRMWARE_DIRECTORY)
     server.serve_forever()
 
 except KeyboardInterrupt:
-    print 'Shutting down httpserver'
+    print('Shutting down httpserver')
     server.socket.close()
